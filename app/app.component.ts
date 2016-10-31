07c34d8de3c9823d7e6174e73641ebdfa2dcb745
l-karnadud <karnadudlera@gmail.com>
2016-10-31 09:26:25 +0300

    add routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
+import {ContactComponent} from './contacts/contacts.component';
+import {FoldersComponent} from './folders/folders.component'
+import {ROUTER_DIRECTIVES} from '@angular/router';
+//import {RouteConfig} from '@angular/router';
 
 
 @Component({
-    selector: 'todo-app',
+    selector: 'my-app',
     templateUrl: './app/app.component.html',
-    styleUrls: ['./app/app.component.css']
+    styleUrls: ['./app/app.component.css'],
+    directives: [ContactComponent, FoldersComponent, ROUTER_DIRECTIVES]
 })
+/*
+@RouteConfig([
+    {path: '/contacts', name: 'Contacts', component: ContactComponent}
+])
+*/
 export class AppComponent {
    // title: string;
 
---
+++ app/folders/folders.component.ts
@@ -0,0 +1,20 @@
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'my-folders',
+    templateUrl: './app/folders/folders.component.html',
+    styleUrls: ['./app/app.component.css'],
+})
+
+export class FoldersComponent {
+
+
+    constructor() {
+      /*  var print = $.getJSON( "folder/" + 1 + ".json", function(data) {
+            alert(data);
+        }); */
+    }
+}
